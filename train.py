3198a55ef075cd0cb619b4d983e2eee08fc1cac7
hemjeet <hemjeetsingh88@gmail.com>
2023-11-05 20:32:39 +0530

    2nd

--- train.py
+++ train.py
@@ -53,38 +53,36 @@ X = np.arange(-100, 100, 4)
 # Create labels
 y = np.arange(-90, 110, 4)
 
+# Create features
+X = np.arange(-100, 100, 4)
+
+# Create labels
+y = np.arange(-90, 110, 4)
 
 # Split data into train and test sets
-X_train = X[:40] # first 40 examples (80% of data)
+X_train = X[:40]  # first 40 examples (80% of data)
 y_train = y[:40]
 
-X_test = X[40:] # last 10 examples (20% of data)
+X_test = X[40:]  # last 10 examples (20% of data)
 y_test = y[40:]
 
-
-# Take a single example of X
-input_shape = X[0].shape 
-
-# Take a single example of y
-output_shape = y[0].shape
-
-
 # Set random seed
-tf.random.set_seed(42)
+# tf.random.set_seed(42)
 
 # Create a model using the Sequential API
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(1), 
+    tf.keras.layers.Dense(1, input_shape=(1,)),  # Input shape should match the shape of your X data
     tf.keras.layers.Dense(1)
-    ])
+])
 
 # Compile the model
-model.compile(loss = tf.keras.losses.mae,
-              optimizer = tf.keras.optimizers.SGD(),
-              metrics = ['mae'])
+model.compile(loss=tf.keras.losses.mean_absolute_error,
+              optimizer=tf.keras.optimizers.SGD(),
+              metrics=['mae'])
 
 # Fit the model
 model.fit(X_train, y_train, epochs=100)
+
 
 
 # Make and plot predictions for model_1
@@ -98,5 +96,5 @@ mse_1 = np.round(float(mse(y_test, y_preds.squeeze()).numpy()), 2)
 print(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
 
 # Write metrics to file
-with open('metrics.txt', 'w') as outfile:
+with open('results.txt', 'w') as outfile:
     outfile.write(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
